b20c5def333aae86bef7dabdf271e43f534eca6f
hongnhunng <hongnhunglyzin97@gmail.com>
2019-06-21 11:58:59 +0700

    updated

--- auth_api/funds/models.py
+++ auth_api/funds/models.py
@@ -7,7 +7,7 @@ class Fund(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, unique=True, blank=False, null=False)
     date = models.DateTimeField(default=datetime.now, blank=True)
-    user_story = models.CharField(max_length=500, null=True)
+    works = models.CharField(max_length=500, null=True)
     user_task = models.CharField(max_length=500, null=True)
     content =  models.CharField(max_length=140, default=' ')
     expenses = models.IntegerField(default=0)
